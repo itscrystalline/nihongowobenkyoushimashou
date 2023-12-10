8bc0b2fe91194f2e971842323930ce2396fa2dd3
Fuji <fuji.ashira@gmail.com>
2023-12-10 22:57:27 +0700

    Change QuizSession.InterpretedArgs()
    Because my friend so dumb shit at refactoring

--- quiz.py
+++ quiz.py
@@ -2,14 +2,10 @@ import json
 import random
 import time
 import sys
+import os
 
 from inspect import currentframe
 from colorama import Fore, Back, Style
-
-
-def getLine():
-    cf = currentframe()
-    return cf.f_back.f_lineno
 
 
 class QuizSession:
@@ -21,36 +17,37 @@ class QuizSession:
         self.loadedFile = ""
         self.loadedData = []
         self.questions = []
-        self.settings = {}
 
         self.debug = False
         self.colorize = True
 
-        parsed = self.parseArgs(args)
-        interpreted = self.interpretArgs(parsed)
-        self.settings = interpreted
+        self.interpreted = self.InterpretedArgs(args)
 
+    @staticmethod
+    def getLine():
+        cf = currentframe()
+        return cf.f_back.f_lineno
+
+    def LoadQuestion(self):
         start = time.time()
-        # Extra argument handling
-
         # Load selected file
-        if "file" in interpreted:
+        if "file" in self.interpreted:
             # Clear score data
-            if "toClear" in interpreted:
-                print(self.col(Fore.YELLOW) + "Clearing scores from", interpreted["file"] + self.col(Fore.RESET))
-                self.loadSet(interpreted["file"])
-                for card in self.loadedData: # card in cards
+            if self.interpreted["toClear"]:
+                print(self.col(Fore.YELLOW) + "Clearing scores from", self.interpreted["file"] + self.col(Fore.RESET))
+                self.loadSet(self.interpreted["file"])
+                for card in self.loadedData:  # card in cards
                     card["score"] = 0
-                self.saveSet(interpreted["file"])
+                self.saveSet(self.interpreted["file"])
                 print(self.col(Fore.GREEN) + "Done!" + self.col(Fore.RESET))
                 quit(0)
-            file = interpreted["file"]
-            questionAmount = interpreted["numCards"] if "numCards" in interpreted else 20
+            file = self.interpreted["file"]
+            questionAmount = self.interpreted["numCards"] if "numCards" in self.interpreted else 20
 
         # Change path
         # If dir command exists
-        elif "dir" in interpreted:
-            dirName = interpreted["dir"]
+        elif "dir" in self.interpreted:
+            dirName = self.interpreted["dir"]
             files = [[dirName + "N5.json", 20], [dirName + "N4.json", 10], [dirName + "N3.json", 5]]
             fileWeights = [0.5, 0.25, 0.125]
 
@@ -68,7 +65,7 @@ class QuizSession:
             questionAmount = result[1]
 
         # Load the json file
-        self.debugPrint("Loading", file, "with", questionAmount, "random cards", getLine())
+        self.debugPrint("Loading", file, "with", questionAmount, "random cards", self.getLine())
         print(self.col(Fore.CYAN), "=========>", file[:-5], f"({questionAmount} questions)", "<=========")
         self.loadedFile = file
         self.loadSet(file)
@@ -76,10 +73,10 @@ class QuizSession:
         # Pick random cards
         randomCards = self.getCardsRandom(questionAmount)
         self.questions = self.getQuestions(randomCards)
-        self.debugPrint(self.questions, getLine())
+        self.debugPrint(self.questions, self.getLine())
 
         end = time.time()
-        self.debugPrint("Preparation done in", end - start, "ms.", getLine())
+        self.debugPrint("Preparation done in", end - start, "ms.", self.getLine())
 
     # Give the command line color
     def col(self, code: str) -> str:
@@ -98,11 +95,11 @@ class QuizSession:
             cards: list = data["cards"]
             self.loadedData = cards
 
-            self.debugPrint("Loaded", cards.__len__(), "cards total", getLine())
+            self.debugPrint("Loaded", cards.__len__(), "cards total", self.getLine())
 
     # Not saving score
     def saveSet(self, target=None) -> None:
-        if "dryRun" in self.settings and "toClear" not in self.settings:
+        if self.interpreted["dryRun"] and not self.interpreted["toClear"]:
             print(self.col(Fore.YELLOW) + "Dry run enabled, not saving!" + self.col(Fore.RESET))
             return
 
@@ -140,19 +137,19 @@ class QuizSession:
         cards = []
         for index, card in enumerate(self.loadedData):
             indices.append(index)
-            if "reverseWeights" in self.settings:
-                scores.append(card["score"]*-1 + 0.1)
+            if self.interpreted["reverseWeights"]:
+                scores.append(card["score"] * -1 + 0.1)
             else:
                 scores.append(card["score"] + 0.1)
         sum_scores = sum(scores)
-        self.debugPrint("sum scores:", sum_scores, getLine())
+        self.debugPrint("sum scores:", sum_scores, self.getLine())
         weights = [(score / sum_scores) * 100 for score in scores]
         del sum_scores
-        self.debugPrint("Scores:", scores, getLine())
-        self.debugPrint("Weights:", weights, getLine())
-        self.debugPrint("Indices:", indices, getLine())
+        self.debugPrint("Scores:", scores, self.getLine())
+        self.debugPrint("Weights:", weights, self.getLine())
+        self.debugPrint("Indices:", indices, self.getLine())
         randomIndices = random.choices(indices, weights=weights, k=questionAmount)
-        self.debugPrint("Random Indices:", randomIndices, getLine())
+        self.debugPrint("Random Indices:", randomIndices, self.getLine())
         for index in randomIndices:
             cards.append(self.getCard(index))
 
@@ -185,29 +182,21 @@ class QuizSession:
         return questionsToReturn
 
     # Command prompt/ Terminal args
-    def parseArgs(self, args: list) -> list[list[str]]:
+    def InterpretedArgs(self, args: list[str]) -> dict[str, str]:
         self.debug = "--debug" in args or "-d" in args if len(sys.argv) > 1 else False
-        self.debugPrint("Debug mode enabled!", getLine())
-        self.debugPrint("Raw Arguments:", args, getLine())
-
-        parsedArgs = []
-        for arg in args:
-            if arg.startswith("-"):
-                parsedArgs.append([arg])
-            else:
-                parsedArgs[-1].append(arg)
-
-        self.debugPrint("Parsed Arguments:", parsedArgs, getLine())
-        return parsedArgs
-
-    def interpretArgs(self, args: list[list[str]]) -> dict[str, str]:
+        self.debugPrint("Debug mode enabled!", self.getLine())
+        self.debugPrint("Raw Arguments:", args, self.getLine())
         interpreted = {}
-        for argGroup in args:
-            if argGroup[0] == "--debug" or argGroup[0] == "-d":
+        for index, argGroup in enumerate(args):
+            if not argGroup.startswith("-"):
                 continue
-            elif argGroup[0] == "--clear":
+            if argGroup in ["--debug", "-d"]:
+                continue
+            if argGroup == "--clear":
                 interpreted["toClear"] = True
-            elif argGroup[0] == "--help" or argGroup[0] == "-h":
+            else:
+                interpreted["toClear"] = False
+            if argGroup in ["--help", "-h"]:
                 print("Usage: python quiz.py [options]")
                 print("Options:")
                 print("  --help, -h: Display this help message.")
@@ -223,53 +212,47 @@ class QuizSession:
                 print("  --reverse-weights, -r: Reverse the weights for the random card selection. This will make "
                       "cards with higher scores more likely to be chosen.")
                 quit(0)
-            elif argGroup[0] == "--dir" or argGroup[0] == "-D":
-                if len(argGroup) < 2:
+            if argGroup in ["--dir", "-D"]:
+                if not os.path.isdir(args[index + 1]):
                     print("FATAL: No directory name provided to load!")
                     quit(1)
 
-                if not argGroup[1].endswith("/"):
-                    argGroup[1] += "/"
+                if not args[index + 1].endswith("/"):
+                    args[index + 1] += "/"
 
-                dirName = argGroup[1]
-                interpreted["dir"] = dirName
-            elif argGroup[0] == "--file" or argGroup[0] == "-f":
-                if len(argGroup) < 2:
+                interpreted["dir"] = args[index + 1]
+            if argGroup in ["--file", "-f"]:
+                if not os.path.isfile(args[index + 1]):
                     print("FATAL: No file name provided to load!")
                     quit(1)
 
-                if not argGroup[1].endswith(".json"):
+                if not args[index + 1].endswith(".json"):
                     print("FATAL: File name provided is not a .json file!")
                     quit(1)
 
-                fileName = argGroup[1]
-                interpreted["file"] = fileName
+                interpreted["file"] = args[index + 1]
 
-            elif argGroup[0] == "--num-cards" or argGroup[0] == "-n":
-                if len(argGroup) < 2:
-                    print("FATAL: No file name provided to load!")
+            if argGroup in ["--num-cards", "-n"]:
+                if not args[index + 1]:
+                    print("FATAL: No number of cards was provided!")
                     quit(1)
 
                 try:
-                    numCards = int(argGroup[1])
+                    interpreted["numCards"] = int(args[index + 1])
                 except ValueError:
                     print("FATAL: Number of cards provided is not an integer!")
                     quit(1)
 
-                interpreted["numCards"] = numCards
-
-            elif argGroup[0] == "--no-colorize" or argGroup[0] == "-N":
-                interpreted["noColorize"] = True
+            if argGroup in ["--no-colorize", "-N"]:
                 self.colorize = False
 
-            elif argGroup[0] == "--dry-run":
+            if argGroup == "--dry-run":
                 interpreted["dryRun"] = True
 
-            elif argGroup[0] == "--reverse-weights" or argGroup[0] == "-r":
+            if argGroup in ["--reverse-weights", "-r"]:
                 interpreted["reverseWeights"] = True
             else:
-                print("WARNING: Unknown argument:", argGroup[0], "Continuing...")
-                continue
+                interpreted["reverseWeights"] = False
 
         return interpreted
 
@@ -303,7 +286,7 @@ def mainLoop(session: QuizSession) -> None:
         while not isValid:
             userAnswer = input(session.col(Fore.CYAN) + "Answer (1-4, n for don't know): " + session.col(Fore.RESET))
             isValid = userAnswer == "q" or userAnswer == "n" or (userAnswer.isdigit() and 1 <= int(userAnswer) <= 4)
-            print(session.col(Fore.RED) + "Invalid input!" + session.col(Fore.RESET)) if not isValid else None;
+            print(session.col(Fore.RED) + "Invalid input!" + session.col(Fore.RESET)) if not isValid else None
         timeBonus = time.perf_counter() - timeBonus
         print(session.col(Fore.LIGHTYELLOW_EX) + "You used", timeBonus, "second(s)" + session.col(Fore.RESET))
         totalTime += timeBonus
@@ -312,16 +295,16 @@ def mainLoop(session: QuizSession) -> None:
         elif timeBonus < 6:
             timeBonus = 2
         else:
-            timeBonus = (16-timeBonus)/5
+            timeBonus = (16 - timeBonus) / 5
         if userAnswer == "q":
             print(session.col(Fore.RED) + "You quit early!" + session.col(Fore.RESET))
-            print(session.col(Fore.LIGHTYELLOW_EX) + "You used a total of", totalTime, "second(s) to answered", correctAnswers
-          , "questions correctly out of",num,"questions." + session.col(Fore.RESET))
+            print(session.col(Fore.LIGHTYELLOW_EX) + "You used a total of", totalTime, "second(s) to answered",
+                  correctAnswers, "questions correctly out of", num, "questions." + session.col(Fore.RESET))
             quitEarly = True
             break
 
         if userAnswer == "n":
-            print(session.col(Fore.LIGHTRED_EX), question["score"], "->", question["score"] - 1,
+            print(session.col(Fore.LIGHTRED_EX) + "Don't know:", question["score"], "->", question["score"] - 1,
                   session.col(Fore.RESET))
             print(session.col(Fore.GREEN) + "Correct answer: ", session.col(Style.BRIGHT), correctAnswerIndex, ". ",
                   question["correct"], session.col(Fore.RESET),
@@ -347,13 +330,15 @@ def mainLoop(session: QuizSession) -> None:
             if question["score"] > -10:
                 question["score"] -= timeBonus
             session.setCardScore(question["index"], question["score"])
-    print(session.col(Fore.LIGHTYELLOW_EX) + "You used a total of", totalTime, "second(s) to answered", correctAnswers
-          , "questions correctly out of",len(questions),"questions." + session.col(Fore.RESET)) if not quitEarly else None
+    print(session.col(Fore.LIGHTYELLOW_EX) + "You used a total of", totalTime, "second(s) to answer", correctAnswers,
+          "questions correctly out of", len(questions),
+          "questions." + session.col(Fore.RESET)) if not quitEarly else None
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     session = QuizSession(args)
+    session.LoadQuestion()
 
     mainLoop(session)
 
